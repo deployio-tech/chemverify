fe15d1cbcabcf5c7bd56edc1b850e8b10f884db1
vasudev <vasujoshi2003@gmail.com>
2026-02-14 23:02:20 +0530

    added the get user details for the prolife page

--- server/src/main/java/com/fyp/server/controller/UserController.java
+++ server/src/main/java/com/fyp/server/controller/UserController.java
@@ -1,11 +1,14 @@
 package com.fyp.server.controller;
 
 import com.fyp.server.dto.SignUpRequestDTO;
+import com.fyp.server.dto.UserProfileDTO;
 import com.fyp.server.entity.User;
 import com.fyp.server.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,6 +21,20 @@ public class UserController {
         this.userService = userService;
     }
 
+    @GetMapping("/me")
+    public ResponseEntity<UserProfileDTO> getCurrentUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = (User) authentication.getPrincipal();
+
+        UserProfileDTO profile = UserProfileDTO.builder()
+                .id(user.getId())
+                .name(user.getName())
+                .email(user.getEmail())
+                .roles(user.getRoles())
+                .build();
+
+        return ResponseEntity.ok(profile);
+    }
 
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody SignUpRequestDTO req) {
