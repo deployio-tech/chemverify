39f93b2f70d0d48d5c2ece2bf32f20231f9a3686
vasudev <vasujoshi2003@gmail.com>
2026-03-12 22:34:01 +0530

    added the login response dto

--- server/src/main/java/com/fyp/server/dto/loginResponseDTO.java
+++ server/src/main/java/com/fyp/server/dto/loginResponseDTO.java
@@ -3,6 +3,7 @@ package com.fyp.server.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @Data
 @AllArgsConstructor
