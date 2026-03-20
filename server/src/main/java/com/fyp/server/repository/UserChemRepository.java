aedff3f3039724411de417041b7cd9c492b11431
vasudev <vasujoshi2003@gmail.com>
2026-03-20 23:02:13 +0530

    added method to find UserChem by userId and order by createdAt descending

--- server/src/main/java/com/fyp/server/repository/UserChemRepository.java
+++ server/src/main/java/com/fyp/server/repository/UserChemRepository.java
@@ -3,5 +3,8 @@ package com.fyp.server.repository;
 import com.fyp.server.entity.UserChem;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface UserChemRepository extends MongoRepository<UserChem,String> {
+    List<UserChem> findByUserIdOrderByCreatedAtDesc(String userId);
 }
