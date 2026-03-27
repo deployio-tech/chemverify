74367e44e450a1c4b2de1ff4a63fd6eb8cf61a9d
vasudev <vasujoshi2003@gmail.com>
2026-03-27 22:57:26 +0530

    add connection to mongodb for recommendation

--- services/config/db.py
+++ services/config/db.py
@@ -6,8 +6,20 @@ from pymongo import MongoClient
 load_dotenv()
 
 MONGO_URI = os.getenv("MONGO_URI")
+MONGO_URI_2 = os.getenv("MONGO_URI_2")
 
 client = MongoClient(MONGO_URI)
 
+client2 = MongoClient(MONGO_URI_2)
+
 def get_db():
     return client["rag_db"]
+
+def get_ingredient_db():
+    return client2["ingredient_mapping"]
+
+def get_product_formulation_db():
+    return client2["product_formulation"]
+
+def get_concentration_db():
+    return client2["concentration_mapping"]
